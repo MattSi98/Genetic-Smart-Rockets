61f060627a128dd81848b4a405677030ae39d60d
nar73 <39714385+Nrobinson57@users.noreply.github.com>
2019-11-08 14:42:00 -0500

    Added UI

---
+++ Smart Rockets/Assets/Scripts/GenerationDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class GenerationDisplay : MonoBehaviour
+{
+
+    public int currentGeneration;
+    public Text genText;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        GameObject main = GameObject.Find("MainEasy");
+        
+        MainEasy mainE = main.GetComponent<MainEasy>();
+        currentGeneration = mainE.currentGen; 
+        
+    }
+
+    void Update()
+    {
+        Debug.Log("Generation: " + currentGeneration);
+    genText.text = "Generation: " + 5;
+        
+    }
+}
