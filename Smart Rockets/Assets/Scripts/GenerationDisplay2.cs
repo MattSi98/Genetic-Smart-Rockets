cefcd3771bc2aa9eaccc3e31aec58cc253343822
nar73 <39714385+Nrobinson57@users.noreply.github.com>
2019-11-18 18:08:18 -0500

    fixed start menu UI

---
+++ Smart Rockets/Assets/Scripts/GenerationDisplay2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerationDisplay2 : MonoBehaviour
+{
+    public int currentGeneration;
+    public Text genText;
+    private MainMed mainMed;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            Debug.Log("tag: " + child.tag);
+            if (child.tag == "Main")
+                mainMed = child.gameObject.GetComponent<MainMed>();
+        }
+
+    }
+
+    void Update()
+    {
+        genText.text = "Generation: " + mainMed.currentGen;
+
+    }
+}
+
+
+
+
