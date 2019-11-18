af49e61314497400cd6ca2698a0816ddbf319c42
nar73 <39714385+Nrobinson57@users.noreply.github.com>
2019-11-18 17:48:29 -0500

    Making start screen

---
+++ Smart Rockets/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+   public void GotoEasyMapScene()
+    {
+        SceneManager.LoadScene("Easy Map");
+    }
+
+    public void GotoMediumMapScene()
+    {
+        SceneManager.LoadScene("Medium Map");
+    }
+
+    public void GotoHardMapScene() 
+    {
+        SceneManager.LoadScene("Hard Map");
+    
+    }
+
+}
